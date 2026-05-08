a65a7f5823f7771534e52e18347b0b4fd2994d22
Forge <forge@gregagi.com>
2026-05-08 15:38:41 +0000

    Update BarShelf cask to 0.3.9

--- Casks/barshelf.rb
+++ Casks/barshelf.rb
@@ -1,6 +1,6 @@
 cask "barshelf" do
-  version "0.3.8"
-  sha256 "096fa40c4c268cf76ec2b7a2d77635011fbdf7b6c0983eb3f9fccf6fa677e705"
+  version "0.3.9"
+  sha256 "7a9e8615aa9dc4ee22437201776903df6057f619af5047618106b9d1b7857c7a"
 
   url "https://github.com/LVTD-LLC/barshelf/releases/download/v#{version}/BarShelf.dmg"
   name "BarShelf"
